6af1ebe48f2a40ac9b356a4cce4de112e66b8774
Sairam Konuru <sairam.konuru@gmail.com>
2018-01-20 17:20:57 +0530

    PS Read CSV

---
+++ ps/readCSV.ps1
@@ -0,0 +1,24 @@
+﻿try
+{
+    $FilePath = "C:\Users\Sairam\desktop\myCsv.csv"
+    $headder = @("first_name","last_name","env_type")
+    $records = Import-Csv  $FilePath -Header $headder
+    $env = "UAT"
+
+    [int]$i = [int]$records.Count
+     Write-Host "The Count: $i"
+    if($i -gt 0)
+    {       
+        $records | Foreach-Object{        
+            [string]$env_type = [string]$_.env_type 
+            if($env_type.Contains( $env) -or ($env_type -eq "all") )
+            {
+                Write-Host $env_type       
+            }
+        }
+    }    
+}
+catch
+{
+ Write-Host $_.Exception.Message
+}
