cabd7c508f21a7ea2cea5ad7c6dd78c2c7856c65
Sairam Konuru <sairam.konuru@gmail.com>
2018-01-28 18:42:26 +0530

    Update Script1.ps1

--- ps/Script1.ps1
+++ ps/Script1.ps1
@@ -7,6 +7,7 @@ Import-Module .\PowerShellModuleProject1.psm1
 SayHello
 
 
-
+#Get-CimInstance Win32_Process -Filter "name = 'FireFox.exe'" | fl name,CommandLine
+#https://blogs.technet.microsoft.com/heyscriptingguy/2015/02/27/get-process-owner-and-other-info-with-wmi-and-powershell/
 
 SayHello
